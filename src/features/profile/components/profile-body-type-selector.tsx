36ee99f06937b366a5a8b90f2f263e07a3a69afd
suzuki3jp <suzuki3.info@gmail.com>
2025-11-01 20:49:01 +0900

    プロフィール実装のガイドを作成

---
+++ src/features/profile/components/profile-body-type-selector.tsx
@@ -0,0 +1,13 @@
+interface ProfileBodyTypeSelectorProps {
+  isMyProfile: boolean;
+}
+
+export function ProfileBodyTypeSelector({
+  isMyProfile,
+}: ProfileBodyTypeSelectorProps) {
+  // MyProfileBodyTypeSelector と OtherProfileBodyTypeSelector を条件分岐で切り替える
+}
+
+function MyProfileBodyTypeSelector() {}
+
+function OtherProfileBodyTypeSelector() {}
---
+++ src/features/profile/components/profile-header.tsx
@@ -0,0 +1,3 @@
+// このコンポーネントには遊佐ーネームとかアイコンとかのエリアを担当しそう
+// profile-follow-button.tsx, profile-body-type-selector.tsx などと組み合わせて使う想定
+export function ProfileHeader() {}
