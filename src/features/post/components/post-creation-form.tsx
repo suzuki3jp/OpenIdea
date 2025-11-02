e114dff0232835f912a9d4a84c72f02244920c02
ryotaKFC <ryotamokatoka@gmail.com>
2025-11-02 23:50:34 +0900

    一度だけ送信できるようにした バリデーションチェック通らないとボタンを押せないように

--- src/features/post/components/post-creation-form.tsx
+++ src/features/post/components/post-creation-form.tsx
@@ -1,76 +1,132 @@
-"use client"
+"use client";
 
-import * as v from 'valibot';
-import { createPost } from "../actions/create-post"
-import { postFreeContent, postPaidContent, postTitleSchema } from "../schemas";
-import { useState } from 'react';
+import { type Dispatch, type SetStateAction, useState } from "react";
+import * as v from "valibot";
+import { createPost } from "../actions/create-post";
+import {
+  postFreeContentSchema,
+  postPaidContentSchema,
+  postTitleSchema,
+} from "../schemas";
 
 export function PostCreationForm() {
+  const [title, setTitle] = useState("");
+  const [freeContent, setFreeContent] = useState("");
+  const [paidContent, setPaidContent] = useState("");
 
-    async function handleAction(formData: FormData): Promise<void> {
-        await createPost(formData);
-    }
+  const [isSubmmitting, setIsSubmitting] = useState(false);
+  const [isTitleError, setIsTitleError] = useState(true);
+  const [isFreeContentError, setIsFreeContentError] = useState(true);
+  const [isPaidContentError, setIsPaidContentError] = useState(true);
 
-    return (
-        <div className="mx-10 my-10">
-            <form action={handleAction} className="flex flex-col items-center space-y-4">
-                <div className="flex flex-col p-6 space-y-5 bg-[#FFFEEE] shadow-xl rounded-xl w-full max-w-2xl">
-                    <Input label="タイトル" name="title" schema={postTitleSchema}/>
-                    <Textarea label="無料テキスト" name="freeContent" schema={postFreeContent}></Textarea>
-                    <Textarea label="有料テキスト" name="paidContent" schema={postPaidContent}></Textarea>
-                </div>
-                <button type="submit" className="py-2 w-1/2 max-w-sm shadow-xl rounded-full bg-linear-to-r from-[#FFF7A0] to-[#FFAFAB]">投稿する</button>
-            </form>
+  const isDisabledButton =
+    (!title && !freeContent && !paidContent) ||
+    isTitleError ||
+    isFreeContentError ||
+    isPaidContentError ||
+    isSubmmitting;
+
+  async function handleAction(formData: FormData): Promise<void> {
+    await createPost(formData);
+    setIsSubmitting(false);
+  }
+
+  return (
+    <div className="mx-10 my-10">
+      <form
+        action={handleAction}
+        onSubmit={() => setIsSubmitting(true)}
+        className="flex flex-col items-center space-y-4"
+      >
+        <div className="flex w-full max-w-2xl flex-col space-y-5 rounded-xl bg-[#FFFEEE] p-6 shadow-xl">
+          <Input
+            variant="input"
+            label="タイトル"
+            name="title"
+            setValue={setTitle}
+            setIsError={setIsTitleError}
+            schema={postTitleSchema}
+          />
+          <Input
+            variant="textarea"
+            label="無料テキスト"
+            name="freeContent"
+            setValue={setFreeContent}
+            setIsError={setIsFreeContentError}
+            schema={postFreeContentSchema}
+          />
+          <Input
+            variant="textarea"
+            label="有料テキスト"
+            name="paidContent"
+            setValue={setPaidContent}
+            setIsError={setIsPaidContentError}
+            schema={postPaidContentSchema}
+          />
         </div>
-    )
+        <button
+          type="submit"
+          disabled={isDisabledButton}
+          className="w-1/2 max-w-sm rounded-full bg-linear-to-r from-[#FFF7A0] to-[#FFAFAB] py-2 shadow-xl hover:cursor-pointer disabled:cursor-not-allowed"
+        >
+          投稿する
+        </button>
+      </form>
+    </div>
+  );
 }
 
 type Props = {
-    label: string,
-    name: string,
-    schema: v.BaseSchema<any, any, any>,
-}
+  variant: "input" | "textarea";
+  label: string;
+  name: string;
+  setValue: Dispatch<SetStateAction<string>>;
+  setIsError: Dispatch<SetStateAction<boolean>>;
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  schema: v.BaseSchema<any, any, any>;
+};
 
-function Input({label, name, schema}: Props) {
-    const [error, setError] = useState("");
+function Input({ variant, label, name, setValue, setIsError, schema }: Props) {
+  const [error, setError] = useState("");
 
-    function handleChange(e:React.ChangeEvent<HTMLInputElement>) {
-        const value = e.target.value;
-        const result = v.safeParse(schema, value);
-        if(!result.success) {
-            setError(result.issues[0].message);
-        } else { 
-            setError("");
-        }
+  function handleChange(
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>,
+  ) {
+    const value = e.target.value;
+    setValue(value);
+    const result = v.safeParse(schema, value);
+    if (!result.success) {
+      setError(result.issues[0].message);
+      setIsError(true);
+    } else {
+      setError("");
+      setIsError(false);
     }
+  }
 
-    return(
-        <div className="flex flex-col">
-            <label htmlFor={name}>{label}</label>
-            <input type="text" name={name} id={name} onChange={(e) => handleChange(e)} className="shadow-inner rounded-md bg-linear-to-r from-[#F7FBFD] to-[#E3F2F4] "/>
-            {error && <p className="text-red-500">{error}</p>}
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col">
+      <label htmlFor={name}>{label}</label>
+      {variant === "input" ? (
+        <input
+          type="text"
+          name={name}
+          id={name}
+          onChange={(e) => handleChange(e)}
+          className="rounded-md bg-linear-to-r from-[#F7FBFD] to-[#E3F2F4] shadow-inner"
+        />
+      ) : (
+        <textarea
+          name={name}
+          id={name}
+          onChange={(e) => handleChange(e)}
+          className="rounded-md bg-linear-to-r from-[#F7FBFD] to-[#E3F2F4] shadow-inner"
+        ></textarea>
+      )}
 
-function Textarea({label, name, schema}: Props){
-    const [error, setError] = useState("");
-
-    function handleChange(e:React.ChangeEvent<HTMLTextAreaElement>) {
-        const val = e.target.value;
-        const result = v.safeParse(schema, val);
-        if(!result.success) {
-            setError(result.issues[0].message);
-        } else {
-            setError("");
-        }
-    }
-    
-    return(
-        <div className="flex flex-col">
-            <label htmlFor={name}>{label}</label>
-            <textarea name={name} id={name} onChange={(e) => handleChange(e)} className="shadow-inner rounded-md bg-linear-to-r from-[#F7FBFD] to-[#E3F2F4] "></textarea>
-            {error && <p className="text-red-500">{error}</p>}
-        </div>
-    )
+      {error && <p className="text-red-500">{error}</p>}
+    </div>
+  );
 }
