d33ba8ee21c1cd4fb90ba1335084fe1ef52bdb0e
ryotaKFC <ryotamokatoka@gmail.com>
2025-11-03 12:28:46 +0900

    biome効かせました

--- src/app/post/create/page.tsx
+++ src/app/post/create/page.tsx
@@ -2,10 +2,10 @@ import { PostCreationForm } from "@/features/post/components/post-creation-form"
 import { PostCreationHeader } from "@/features/post/components/post-creation-header";
 
 export default function () {
-    return(
-        <>
-            <PostCreationHeader />
-            <PostCreationForm />
-        </>
-    );
+  return (
+    <>
+      <PostCreationHeader />
+      <PostCreationForm />
+    </>
+  );
 }
--- src/features/post/components/back-button.tsx
+++ src/features/post/components/back-button.tsx
@@ -1,13 +1,11 @@
-"use client"
+"use client";
 
-import { ChevronLeft } from "lucide-react"
-
-
+import { ChevronLeft } from "lucide-react";
 
 export function BackButton() {
-    return(
-        <div className="my-auto ml-12">
-            <ChevronLeft height={50} width={50} />
-        </div>
-    )
+  return (
+    <div className="my-auto ml-12">
+      <ChevronLeft height={50} width={50} />
+    </div>
+  );
 }
