e87f4a63ffff9ef90b5450811f380f3e19175a33
ryotaKFC <ryotamokatoka@gmail.com>
2025-11-03 12:30:37 +0900

    Biome効かせました。まとまりごとに改行も

--- src/features/post/actions/create-post.ts
+++ src/features/post/actions/create-post.ts
@@ -1,39 +1,41 @@
-"use server"
+"use server";
 
-import * as v from 'valibot';
-import { postSchema } from '../schemas';
-import { createClient } from '@/lib/supabase/server'; 
+import * as v from "valibot";
+import { createClient } from "@/lib/supabase/server";
+import { postSchema } from "../schemas";
 
 export async function createPost(formData: FormData) {
-    const supabase = await createClient();
-    const { data: { user } } = await supabase.auth.getUser();
-    if (!user) {
-        return { success: false, message: "認証が必要です" };
-    }
-    const rawData = {
-        title: formData.get("title"),
-        freeContent: formData.get("freeContent"),
-        paidContent: formData.get("paidContent"),
-    }
-    try {
-        const validatedData = v.parse(postSchema, rawData);
+  const supabase = await createClient();
 
-        const {data, error} = await supabase
-            .from("Posts")
-            .insert({
-                user_id: user.id,
-                post_title: validatedData.title,
-                free_content: validatedData.freeContent,
-                paid_content: validatedData.paidContent,
-                good_count: 0,
-                bad_count: 0,
-            })
-        if (error) {
-            // biome-ignore lint/suspicious/noConsole: <explanation>
-            console.error("supabase insert error",error);
-        }
-    } catch (error) {
-        // biome-ignore lint/suspicious/noConsole: <explanation>
-        console.error(error);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    return { success: false, message: "認証が必要です" };
+  }
+
+  const rawData = {
+    title: formData.get("title"),
+    freeContent: formData.get("freeContent"),
+    paidContent: formData.get("paidContent"),
+  };
+  try {
+    const validatedData = v.parse(postSchema, rawData);
+
+    const { data, error } = await supabase.from("Posts").insert({
+      user_id: user.id,
+      post_title: validatedData.title,
+      free_content: validatedData.freeContent,
+      paid_content: validatedData.paidContent,
+      good_count: 0,
+      bad_count: 0,
+    });
+    if (error) {
+      // biome-ignore lint/suspicious/noConsole: <explanation>
+      console.error("supabase insert error", error);
     }
+  } catch (error) {
+    // biome-ignore lint/suspicious/noConsole: <explanation>
+    console.error(error);
+  }
 }
