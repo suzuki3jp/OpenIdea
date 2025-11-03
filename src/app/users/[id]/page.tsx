20d6493ecbc43cace61ba3f966d696642231b367
Rom213husai <cejk1240196@gn.iwasaki.ac.jp>
2025-11-03 12:25:17 +0900

    users/[id]にアクセスしたときに そのページのユーザと現在ログインしているユーザのprofile情報を取得できるように

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,7 +1,24 @@
-export default function ({ params }: PageProps<"/users/[id]">) {
-  // サーバー用の supabase のクライアントを init して、ユーザー情報を取得する
-  // 取得したユーザー情報が [id] と一致する場合にのみ、購入履歴とか本人しか見れない情報を表示する
-  // 違う or ログインしていない場合は、フォローとか一般公開情報のみ表示する
-  // ログインしていない場合は フォローボタンは リンクになっていて、ログインページに飛ばす
-  // profile-header.tsx, profile-body.tsx などを呼ぶ
+import { getCurrentUser } from "@/features/auth/get-current-user";
+import { getUserById } from "@/features/auth/get-user-by-id";
+import { ProfileBody } from "@/features/profile/components/profile-body";
+import { ProfileHeader } from "@/features/profile/components/profile-header";
+import { createClient } from "@/lib/supabase/server";
+
+export default async function ({ params }: PageProps<"/users/[id]">) {
+  const userId = (await params).id;
+  const client = await createClient();
+  const currentUser = await getCurrentUser(client);
+  const user = await getUserById(userId, client);
+
+  return (
+    <>
+      <ProfileHeader user={user} currentUserId={currentUser?.id ?? null} />
+      <ProfileBody user={user} currentUserId={currentUser?.id ?? null} />
+    </>
+  );
 }
+// サーバー用の supabase のクライアントを init して、ユーザー情報を取得する
+// 取得したユーザー情報が [id] と一致する場合にのみ、購入履歴とか本人しか見れない情報を表示する
+// 違う or ログインしていない場合は、フォローとか一般公開情報のみ表示する
+// ログインしていない場合は フォローボタンは リンクになっていて、ログインページに飛ばす
+// profile-header.tsx, profile-body.tsx などを呼ぶ
---
+++ src/features/auth/get-user-by-id.ts
@@ -0,0 +1,13 @@
+import type { SupabaseClient } from "@supabase/supabase-js";
+import { convertDBUserToUser } from "./convert-db-user-to-user";
+
+export async function getUserById(id: string, client: SupabaseClient) {
+  const { data: user, error } = await client
+    .from("Users")
+    .select("*")
+    .eq("user_id", id)
+    .maybeSingle();
+  if (error || !user) return null;
+
+  return convertDBUserToUser(user);
+}
