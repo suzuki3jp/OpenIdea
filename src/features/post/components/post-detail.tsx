4415df2b3e6ed9a28fa08c58781017971cadf13b
ryotaKFC <ryotamokatoka@gmail.com>
2025-11-04 14:21:48 +0900

    セマンティックなタグ 修正もろもろ

--- src/features/post/components/post-detail.tsx
+++ src/features/post/components/post-detail.tsx
@@ -1,12 +1,10 @@
-import { ChevronLeft as BackButtonIcon, Circle } from "lucide-react";
+import { ChevronLeft as BackButtonIcon, Circle as Icon } from "lucide-react";
 import type { Post } from "../types";
 import { ContentLock } from "./content-lock";
 import { PostActions } from "./post-actions";
 import { Tag, type TagType } from "./tag";
 
-{
-  /* TODO: タグテーブルできたらtestHogeは消す */
-}
+/* TODO: タグテーブルできたらtestHogeは消す */
 const testTags: TagType[] = [
   { name: "test1" },
   { name: "test2" },
@@ -15,59 +13,61 @@ const testTags: TagType[] = [
 ];
 const testPaid = false;
 
-type Props = {
+type PostDetailProps = {
   post: Post;
 };
 
-export function PostDetail({ post }: Props) {
+export function PostDetail({ post }: PostDetailProps) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <main className="flex min-h-screen flex-col">
       <BackButtonIcon size={50} className="absolute top-12 left-6" />
-      <Header post={post} tags={testTags} />
-      <Content post={post} isPaid={testPaid} />
-    </div>
+      <PostHeader post={post} tags={testTags} />
+      <PostContent post={post} isPaid={testPaid} />
+    </main>
   );
 }
 
-type HeaderProps = {
+type PostHeaderProps = {
   post: Post;
   tags: TagType[];
 };
 
-function Header({ post, tags }: HeaderProps) {
+function PostHeader({ post, tags }: PostHeaderProps) {
   return (
-    <div className="bg-[#FFFEEE]">
+    <header className="bg-[#FFFEEE]">
       <div className="flex h-full flex-row space-x-5 px-8 pt-32">
         {/* TODO: ユーザー機能が実装されたらアイコンに差し替える */}
-        <Circle size={90} color="gray" fill="gray" />
+        <Icon size={90} color="gray" fill="gray" className="text-amber-700" />
         <div className="flex w-full flex-col space-y-2 pt-4 pr-5">
           <h1 className="w-full text-xl">{post.postTitle}</h1>
-          <div className="border-t-2 border-solid" />
-          <div className="flex flex-wrap gap-2">
+          <hr />
+          <ul className="flex flex-wrap gap-2">
             {tags.map((tag, index) => (
               // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-              <Tag tag={tag} key={index} />
+              <li key={index}>
+                <Tag tag={tag} />
+              </li>
             ))}
-          </div>
+          </ul>
           <PostActions post={post} />
         </div>
       </div>
-    </div>
+    </header>
   );
 }
 
-type ContentProps = {
+type PostContentProps = {
   post: Post;
   isPaid: boolean;
 };
 
-function Content({ post, isPaid }: ContentProps) {
+function PostContent({ post, isPaid }: PostContentProps) {
   return (
     <>
-      <div className="mx-12 my-10">
-        {post.freeContent}
-        {isPaid && <div>{post.paidContent}</div>}
-      </div>
+      <article className="mx-12 my-10">
+        <p>{post.freeContent}</p>
+        {isPaid && <p>{post.paidContent}</p>}
+      </article>
       {!isPaid && <ContentLock />}
     </>
   );
