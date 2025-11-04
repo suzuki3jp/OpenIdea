525be46bdd05ffb3868cd1cc73a1c380bd697d8e
Rom213husai <cejk1240196@gn.iwasaki.ac.jp>
2025-11-04 12:54:25 +0900

    トグルボタンの表示切替と 枠のみ作成

--- src/features/profile/components/profile-body-type-selector.tsx
+++ src/features/profile/components/profile-body-type-selector.tsx
@@ -5,9 +5,25 @@ interface ProfileBodyTypeSelectorProps {
 export function ProfileBodyTypeSelector({
   isMyProfile,
 }: ProfileBodyTypeSelectorProps) {
-  // MyProfileBodyTypeSelector と OtherProfileBodyTypeSelector を条件分岐で切り替える
+  return isMyProfile ? (
+    <MyProfileBodyTypeSelector />
+  ) : (
+    <OtherProfileBodyTypeSelector />
+  );
 }
 
-function MyProfileBodyTypeSelector() {}
+function MyProfileBodyTypeSelector() {
+  return (
+    <div className="mx-auto mt-4 w-[329px] cursor-pointer rounded-full border border-none bg-[#F6F4E0] px-3 py-1 text-[#676767]">
+      My Profile Body Type Selector
+    </div>
+  );
+}
 
-function OtherProfileBodyTypeSelector() {}
+function OtherProfileBodyTypeSelector() {
+  return (
+    <div className="mx-auto mt-4 w-[329px] cursor-pointer rounded-full border-none bg-[#F6F4E0] text-[#676767]">
+      Other User's Profile Body Type Selector
+    </div>
+  );
+}
