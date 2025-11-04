1510de8a8936d06e007295fb12f0d4f9b21fa7d3
Rom213husai <cejk1240196@gn.iwasaki.ac.jp>
2025-11-04 12:35:12 +0900

    googleログイン時に新規ユーザを作成するように変更

--- src/features/auth/routes/google-callback.ts
+++ src/features/auth/routes/google-callback.ts
@@ -9,18 +9,51 @@ export async function GET(request: Request) {
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
+
     if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host");
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
-        return NextResponse.redirect(`${origin}${next}`);
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
-      } else {
-        return NextResponse.redirect(`${origin}${next}`);
+      const userRes = await supabase.auth.getUser();
+      const uid = userRes.data?.user?.id ?? null;
+
+      if (uid) {
+        const { data, error: selectError } = await supabase
+          .from("Users")
+          .select("user_id")
+          .eq("user_id", uid)
+          .maybeSingle();
+
+        if (!selectError && data === null) {
+          const payload = {
+            user_id: uid,
+            display_name: null,
+            icon_path: null,
+            banner_path: null,
+            level: 1,
+            self_introduction: null,
+          };
+
+          const { error: insertError } = await supabase
+            .from("Users")
+            .insert(payload);
+
+          if (insertError && insertError) {
+            // biome-ignore lint/suspicious/noConsole: <explanation>
+            console.error("ユーザー作成エラー", insertError);
+          }
+        }
+
+        const forwardedHost = request.headers.get("x-forwarded-host");
+        const isLocalEnv = process.env.NODE_ENV === "development";
+
+        if (isLocalEnv) {
+          return NextResponse.redirect(`${origin}${next}`);
+        } else if (forwardedHost) {
+          return NextResponse.redirect(`https://${forwardedHost}${next}`);
+        } else {
+          return NextResponse.redirect(`${origin}${next}`);
+        }
       }
     }
-  }
 
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+    return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  }
 }
