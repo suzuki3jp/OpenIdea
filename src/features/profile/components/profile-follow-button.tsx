f266fa7a240f86c3c6a9c47929a39f0d0201f4a2
Rom213husai <cejk1240196@gn.iwasaki.ac.jp>
2025-11-05 19:21:33 +0900

    フォロー・解除の機能を持ったbuttonを実装

--- src/features/profile/components/profile-follow-button.tsx
+++ src/features/profile/components/profile-follow-button.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { handleFollowToggle } from "@/features/follow/actions/handle-follow-toggle";
 export function ProfileFollowButton({
   currentUserId,
   userId,
@@ -5,9 +7,18 @@ export function ProfileFollowButton({
   currentUserId: string | null;
   userId: string | null;
 }) {
+  function onClick() {
+    if (!currentUserId || !userId || currentUserId === userId) {
+      return;
+    }
+
+    const isFollow = handleFollowToggle(currentUserId, userId);
+  }
+
   return (
     <button
       type="button"
+      onClick={onClick}
       className="-mb-1 absolute right-0 h-7 w-[110px] rounded-4xl border-none bg-linear-to-r from-[#A9D6DD] to-[#739ED5] text-[17px] text-white"
     >
       ＋ フォロー
