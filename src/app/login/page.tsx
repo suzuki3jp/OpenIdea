965ac03d27e37110e1973919fdae32dc69d5d0c1
Rom213husai <cejk1240196@gn.iwasaki.ac.jp>
2025-11-06 14:18:24 +0900

    logo画像の差し替えとそれに伴う変更

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { signInWithGoogle } from "@/lib/supabase/auth-google";
-import Logo from "./logo.png";
+import Logo from "./logo.svg";
 
 export default function () {
   const handleGoogleLogin = async () => {
@@ -11,8 +11,8 @@ export default function () {
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-[#FFFEFC]">
-      <div className="mx-auto h-[78.06px] w-[284.69px]">
-        <Image src={Logo} alt="Logo" w-full h-full />
+      <div className="relative mx-auto h-[78.06px] w-[284.69px]">
+        <Image src={Logo} alt="Logo" fill />
       </div>
       <div className="mx-auto mt-[36.94px] h-[123px] w-[289px] rounded-[20px] bg-gradient-to-r from-[#FFFFFF] to-[#FFFEE4] shadow-[2px_4px_4px_rgba(0,0,0,0.25)]">
         <button
